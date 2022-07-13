98bf8f51e2b8f02a086be317d1e1983ff1d8926e
Cintia Arce <cintiaayelenarce@gmail.com>
2022-07-13 19:16:43 -0300

    footer squeleton

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h3>Developer</h3>
+                <span>Cintia Arce</span>
+            </div>
+            <div>
+                <h3>Technologies</h3>
+                <span>React</span>
+                <span>Redux</span>
+            </div>
+        </footer>
+    )
+};
