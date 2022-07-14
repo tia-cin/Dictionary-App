340d0022a9ad2cca50a985fd7748e3a767d66fd8
Cintia Arce <cintiaayelenarce@gmail.com>
2022-07-14 17:39:00 -0300

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { Footer } from './components/Footer';
-import { Navbar } from './components/Navbar';
-
 // api -> https://api.dictionaryapi.dev/api/v2/entries/en
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar/>
-        
-      <Footer/>
+      
     </div>
   );
 }
