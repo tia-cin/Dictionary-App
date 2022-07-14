4cbcad28ddc6ce749e3f43016520fff1ba09427b
Cintia Arce <cintiaayelenarce@gmail.com>
2022-07-14 17:46:28 -0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import { Landing } from './components/Landing';
 // api -> https://api.dictionaryapi.dev/api/v2/entries/en
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/home/:word' />
+      </Routes>
     </div>
   );
 }
