5a94698d83a2cca0b4042cf0a9ec09ea9a77d64a
Cintia Arce <cintiaayelenarce@gmail.com>
2022-07-14 17:36:10 -0300

    Home & Landing files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import './App.css';
-import { Footer } from './components/footer';
+import { Footer } from './components/Footer';
 import { Navbar } from './components/Navbar';
 
 // api -> https://api.dictionaryapi.dev/api/v2/entries/en
@@ -9,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar/>
-
+        
       <Footer/>
     </div>
   );
---
+++ src/components/Landing.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Landing = () => {
+    return (
+        <div>
+            <h1>Welcome to your</h1>
+            <h1>Dictionary</h1>
+        </div>
+    )
+};
