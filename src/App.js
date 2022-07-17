49efe42a2587865e050e0761254a11e08d4b91e1
Cintia Arce <cintiaayelenarce@gmail.com>
2022-07-17 17:10:21 -0300

    failed message for definition not found

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './components/HomePage/Home';
 import { Landing } from './components/Landing';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 // api -> https://api.dictionaryapi.dev/api/v2/entries/en
 const axios = require('axios');
 
 function App() {
   const [ words, setWords ] = useState([]);
+  const [ failMessage, setFailMessage ] = useState({
+    title: "",
+    message: "",
+    resolution: ""
+  });
+
   const searchWords = async (input) => {
     try {
       const req = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${input}`);
       let res = await req.data && req.data.map(r => {
         return {
-              title: r.title ? r.title : null,
-              message: r.message ? r.message : null,
-              resolution: r.resolution ? r.resolution : null,
               word: r.word,
               phonetic: r.phonetic ? r.phonetic : null,
               phonetics: r.phonetics ? r.phonetics : null,
@@ -27,7 +30,12 @@ function App() {
       res ? setWords(w => [...w, res]) : console.log("failed seting words");
       console.log("app res", res);
     } catch (e) {
-      console.log("Request Error: ", e);
+      console.log("Request Error: ", e.response.data);
+      setFailMessage({
+        title: e.response.data.title,
+        message: e.response.data.message,
+        resolution: e.response.data.resolution
+      })
     }
   };
 
@@ -35,7 +43,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={<Landing/>}/>
-        <Route path='/home' element={<Home searchWords={searchWords} words={words}/>} />
+        <Route path='/home' element={<Home searchWords={searchWords} words={words} failMessage={failMessage}/>} />
         <Route path='/home/:word' />
       </Routes>
     </div>
--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -2,12 +2,21 @@ import { Footer } from './Footer';
 import { Navbar } from './Navbar';
 import { CardsContainer } from '../Cards/CardsContainer';
 
-export const Home = ({ words, searchWords }) => {
+export const Home = ({ words, searchWords, failMessage }) => {
     return (
         <div>
             <Navbar searchWords={searchWords}/>
-            {/* <CardsContainer/> */}
             {console.log("home res", words)}
+            {
+                failMessage ? 
+                    <div>
+                        <h2>{failMessage.title}</h2>
+                        <h3>{failMessage.message}</h3>
+                        <p>{failMessage.resolution}</p>
+                    </div>
+                : 
+                    <CardsContainer/>
+            }
             <Footer/>
         </div>
     )
