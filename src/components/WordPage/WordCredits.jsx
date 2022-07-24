fb4c936030add94d5192f4fdff4514bd7da7e682
Cintia Arce <cintiaayelenarce@gmail.com>
2022-07-24 15:23:45 -0300

    word page components fixed

--- src/components/WordPage/WordCredits.jsx
+++ src/components/WordPage/WordCredits.jsx
@@ -3,15 +3,26 @@ import React from "react";
 export const WordCredits = ({ license, sourceUrls }) => {
     return (
         <div>
-            <h2>Credits</h2>
-            <div>
-                <h3>License:</h3>
-                <a href={license.url}><p>{license.name}</p></a>
-            </div>
-            <div>
-                <h3>Sourse URLs:</h3>
-                {sourceUrls && sourceUrls.map(u => <a href={u}>{u}</a>)}
-            </div>
+            {
+                license && sourceUrls ? 
+                <div>
+                    <h2>Credits</h2>
+                    {
+                        license &&
+                        <div>
+                            <h3>License:</h3>
+                            <a href={license.url}><p>{license.name}</p></a>
+                        </div>
+                    }
+                    {sourceUrls && 
+                        <div>
+                            <h3>Sourse URLs:</h3>
+                            {sourceUrls && sourceUrls.map(u => <a href={u} key={u}>{u}</a>)}
+                        </div>
+                    }
+                </div>
+                : null
+            }
         </div>
     )
 };
--- src/components/WordPage/WordPhonetic.jsx
+++ src/components/WordPage/WordPhonetic.jsx
@@ -3,20 +3,26 @@ import React from "react";
 export const WordPhonetic = ({ phonetics }) => {
     return (
         <div>
-            <h2>Phonetics</h2>
-            {phonetics && phonetics.map(p => {
+            
+            {phonetics && <h2>Phonetics</h2> && phonetics.map(p => {
                 return(
                     <section key={Math.random()}>
                         <h5>{p.text}</h5>
-                        <div>
-                            <audio src={p.audio} controls></audio>
-                        </div>
-                        <div>
-                            <h5>License:</h5>
-                            <a href={p.license.url}>
-                                <p>{p.license.name}</p>
-                            </a>
-                        </div>
+                        { 
+                            p.audio && 
+                            <div>
+                                <audio src={p.audio} controls></audio>
+                            </div>
+                        }
+                        {
+                            p.license &&
+                            <div>
+                                <h5>License:</h5>
+                                <a href={p.license.url}>
+                                    <p>{p.license.name}</p>
+                                </a>
+                            </div>
+                        }
                     </section>
                 )
             })}
