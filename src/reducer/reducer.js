1db64d0d432b0b17e3bd2a4f82a6707ad9163360
Cintia Arce <cintiaayelenarce@gmail.com>
2022-07-24 13:20:43 -0300

    reducer & store

---
+++ src/reducer/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    words: [],
+    antonyms: [],
+    synonyms: [],
+    detail: []
+};
+
+export const rootReducer = (state = initialState, { type, action }) => {
+    switch(type) {
+        default: 
+            return state
+    }
+};
