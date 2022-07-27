8a3e082dd8963fe1f1f37af78fa0597c2981a963
Cintia Arce <cintiaayelenarce@gmail.com>
2022-07-27 10:17:04 -0300

    not word found message

--- src/components/Cards/CardsContainer.jsx
+++ src/components/Cards/CardsContainer.jsx
@@ -5,13 +5,18 @@ export const CardsContainer = ({words}) => {
     return (
         <section>
             {
-                words && 
+                words.word ? 
                     words.map((w, i) => {
                         return (
                             <Card key={i} word={w} />
                         )
                     })
-                
+                :
+                    <div>
+                        <h2>{words.title}</h2>
+                        <h3>{words.message}</h3>
+                        <p>{words.resolution}</p>
+                    </div>
             }
         </section>
     )
