a3f21c42a7aeb716e66692cf9508319f273b558a
Cintia Arce <cintiaayelenarce@gmail.com>
2022-08-29 17:18:35 -0300

    style status component

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -2,9 +2,13 @@ import React from "react";
 
 export const Status: React.FC = () => {
   return (
-    <div>
-      <h1>Page under Construcion</h1>
-      <h2>Check again in another time :)</h2>
+    <div style={{ margin: "10em" }}>
+      <h1 style={{ textAlign: "center", fontFamily: "monospace" }}>
+        Page under Construcion
+      </h1>
+      <h2 style={{ textAlign: "center", fontFamily: "monospace" }}>
+        Check again in another time :)
+      </h2>
     </div>
   );
 };
