e2199240a83ce074e4ab8486c01912acfcd2183c
Cintia Arce <cintiaayelenarce@gmail.com>
2022-10-25 19:53:35 -0300

    add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux/es/exports";
 import store from "./redux/store";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
